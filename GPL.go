33e486cea49a78604d619531541be9800de6d372
Conor Kelly <ckelly@ckellydesign.net>
2016-04-06 16:57:14 -0700

    Converting JSON string to Struct for Data Munging!!

--- GPL.go
+++ GPL.go
@@ -5,7 +5,7 @@ import (
   "io/ioutil" // parsing out body of POST
   "log"
   "fmt" // general "console" and output utilities
-  // "encoding/json" // library to parse and unparse JSON
+  "encoding/json" // library to parse and unparse JSON
 
   elastigo "github.com/mattbaird/elastigo/lib" // ElasticSearch Go Library
 )
@@ -47,19 +47,22 @@ func startWebServer() {
 // Routes the POST req.Body into the ElasticSearch channel to be indexed
 func routeReport(rw http.ResponseWriter, req *http.Request) {
   body, _ := ioutil.ReadAll(req.Body)
-  bodyStr := string(body)
+  // bodyStr := string(body)
   // bodyBts := []byte(bodyStr)
+  // var returnStr string
 
-  // var report Report
-  // err := json.Unmarshal(bodyBts, &report)
-  // if err != nil {
-  //   panic(err)
-  // }
+  var report Report
+  err := json.Unmarshal(body, &report)
+  if err != nil {
+    panic(err)
+  }
 
   // report := reports[0]
 
-  // fmt.Printf("\n\nRequest Body: %v", string(body))
-  elastiChan <- bodyStr // send stringified report to ElasticSearch via elastiChan channel
+  // fmt.Printf("\n\nReport Type: %v", report.EnvData.UserAgent)
+  handleReportData(report)
+
+  elastiChan <- string(body) // send stringified report to ElasticSearch via elastiChan channel
 }
 
 // confirm only POST methods are being used
@@ -73,29 +76,65 @@ func postOnly(handle http.HandlerFunc) http.HandlerFunc {
   }
 }
 
+func handleReportData(report Report) {
+  switch report.Type {
+    case "envData":
+      fmt.Printf("\n\nReport Type: %v", report.EnvData.UserAgent)
+    case "adData":
+      report.AdData.AdRequestDelta = report.AdData.AdRequestEnd - report.AdData.AdRequestStart
+      fmt.Printf("AssetData AdRequestDelta : %v", report.AdData.AdRequestDelta)
+      // fmt.Printf("AssetData has Preroll : %v", report.AdData.HasPreroll)
+    case "assetData":
+      fmt.Println("ASSET DATA")
+    case "eventLog":
+      fmt.Println("EVENT LOG")
+  }
+}
 
-// type Report struct {
-//   Type        string            `json:"type"`
-//   Guid        string            `json:"guid"`
-//   RawData     string            `json:"data"`
-//   EnvData     *EnvDataReport
-//   AdData      *AdDataReport
-//   AssetData   *AssetDataReport
-//   EventLog    *EventLogReport
-// }
+type Report struct {
+  Type        string            `json:"type"`
+  Guid        string            `json:"guid"`
+  // RawData     string            
+  EnvData     *EnvDataReport    `json:"envData"`
+  AdData      *AdDataReport     `json:"adData"`
+  AssetData   *AssetDataReport  `json:"assetData"`
+  EventLog    []*EventLogItem   `json:"eventLog"`
+}
 
-// type EnvDataReport struct {
+type EnvDataReport struct {
+  UserAgent       string  `json:"userAgent"`
+  PageURL         string  `json:"pageURL"`
+  PlayerAdapter   string  `json:"playerAdapter"`
+  PlayerVersion   string  `json:"playerVersion"`
+  HasAdBlocker    bool    `json:"hasAdBlocker"`
+  // Date string/float? `json:"date"`
+}
 
-// }
+type AdDataReport struct {
+  AdRequestStart  float64   `json:"adRequestStart"`
+  AdRequestEnd    float64   `json:"adRequestEnd"`
+  AdRequestUrl    string  `json:"adRequestUrl"`
+  HasPreroll      bool    `json:"hasPreroll"`
+  PrerollData     struct {
+    AdId                  string  `json:"_adId"`
+    CreativeId            string  `json:"_creativeId"`
+    CreativeRenditionId   string  `json:"_creativeRenditionId"`
+    SlotCustomId          string  `json:"_slotCustomId"`
+  }                       `json:"prerollData"`
 
-// type AdDataReport struct {
+  // any extra data from parsing middleware
+  AdRequestDelta float64
+}
 
-// }
+type AssetDataReport struct {
+  AssetURL    string  `json:"assetURL"`
+  AssetMPXid  string  `json:"assetMPXid"`
+  AssetGUID   string  `json:"assetGUID"`
+  CcType      string  `json:"ccType"`
+}
 
-// type AssetDataReport struct {
-
-// }
-
-// type EventLogReport struct {
-
-// }
+type EventLogItem struct {
+  Type  string  `json:"type"`
+  Time  float64   `json:"time"`
+  // Delta float this time - previous time
+}
